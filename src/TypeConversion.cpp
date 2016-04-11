7711abbac639a78fc9410e3b725e77b0f30d74f5
Marina Ermakova <m.ermakova77@gmail.com>
2016-04-11 22:24:08 +0300

    Rename bindings

--- src/TypeConversion.cpp
+++ src/TypeConversion.cpp
@@ -6,67 +6,67 @@ namespace utils
     template<>
     std::string getStringByType<clang::FunctionDecl>()
     {
-        return std::string("function");
+        return std::string("FunctionDecl");
     }
 
     template<>
     std::string getStringByType<clang::IfStmt>()
     {
-        return std::string("if");
+        return std::string("IfStmt");
     }
 
     template<>
     std::string getStringByType<clang::ForStmt>()
     {
-        return std::string("for");
+        return std::string("ForStmt");
     }
 
     template<>
     std::string getStringByType<clang::CXXForRangeStmt>()
     {
-        return std::string("range");
+        return std::string("CXXForRangeStmt");
     }
 
     template<>
     std::string getStringByType<clang::WhileStmt>()
     {
-        return std::string("while");
+        return std::string("WhileStmt");
     }
 
     template<>
     std::string getStringByType<clang::DoStmt>()
     {
-        return std::string("doWhile");
+        return std::string("DoStmt");
     }
 
     template<>
     std::string getStringByType<clang::CaseStmt>()
     {
-        return std::string("case");
+        return std::string("CaseStmt");
     }
 
     template<>
     std::string getStringByType<clang::CXXCatchStmt>()
     {
-        return std::string("catch");
+        return std::string("CXXCatchStmt");
     }
 
     template<>
     std::string getStringByType<clang::BinaryOperator>()
     {
-        return std::string("binaryOp");
+        return std::string("BinaryOperator");
     }
 
     template<>
     std::string getStringByType<clang::ConditionalOperator>()
     {
-        return std::string("condOp");
+        return std::string("ConditionalOperator");
     }
 
     template<>
     std::string getStringByType<clang::BinaryConditionalOperator>()
     {
-        return std::string("binaryCondOp");
+        return std::string("BinaryConditionalOperator");
     }
 
 } // namespace utils
